36b9fa0e9e944b2849f2af9fa8abdca37e98b816
JosueRojasGlo <josue.rojas@globant.com>
2024-05-31 11:41:41 -0500

    primerCommit

---
+++ src/main/java/Beta/Gandma/s/Food/Beta/Gradma/s/Food/Application.java
@@ -0,0 +1,13 @@
+package Beta.Gandma.s.Food.Beta.Gradma.s.Food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Beta/Gandma/s/Food/Beta/Gradma/s/Food/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Beta.Gandma.s.Food.Beta.Gradma.s.Food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
